339f17733d0dd8a8409068833764e83178ed2408
TwinsEarth <fangwenxin@thisvr.cn>
2026-09-22 17:12:30 +0800

    v2.0.3: shard_of 哈希均匀分布修复

--- aip-sdk-py/aip/__init__.py
+++ aip-sdk-py/aip/__init__.py
@@ -1,5 +1,5 @@
-"""Agent Universe AIP SDK v2.0.2"""
+"""Agent Universe AIP SDK v2.0.3"""
 from .models import AgentCard, Task, TaskStatus
 from .dht_backend import MemoryDHT
 from .index.sharded import ShardedIndex, ShardMetadata, shard_of
-__version__ = "2.0.2"
+__version__ = "2.0.3"
